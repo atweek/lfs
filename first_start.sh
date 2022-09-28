8145a02f9ab84983ae245d94123b96fded2effd1
atweek <ramille655@gmail.com>
2022-09-28 21:12:08 +0300

    start

---
+++ first_start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install binutils bison coreutils diffutils  findutils gawk gcc grep gzip m4 patch perl python3 sed tar texinfo g++ make vim
+sudo ln -sf bash /bin/sh
