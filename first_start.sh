644f31dbbba4d593a6f43a15f18988e35406bf88
atweek <ramille655@gmail.com>
2022-09-28 21:16:37 +0300

    start

--- first_start.sh
+++ first_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install binutils bison coreutils diffutils  findutils gawk gcc grep gzip m4 patch perl python3 sed tar texinfo g++ make vim
+sudo apt-get update -y
+sudo apt-get -y install binutils bison coreutils diffutils  findutils gawk gcc grep gzip m4 patch perl python3 sed tar texinfo g++ make vim
 sudo ln -sf bash /bin/sh
