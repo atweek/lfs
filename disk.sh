78cd2df63117b8febc6fce7b41bcae49322cd1c0
atweek <ramille655@gmail.com>
2022-10-01 20:33:57 +0300

    disk

--- disk.sh
+++ disk.sh
@@ -3,6 +3,21 @@
 export LFS=/mnt/lfs
 echo $LFS
 
+parted
+mkpart primary ext4 0 500MB
+mkpart primary ext4 500MB 30G
+mkpart primary ext4 30G -0M
+name 1 boot                                                      
+name 2 root                                                      
+name 3 home                                                      
+set 1 boot on
+print
+
+mkfs.ext4 /dev/sdb1
+mkfs.ext4 /dev/sdb2
+mkfs.ext4 /dev/sdb3
+
+
 mkdir -pv $LFS
 mount -v -t ext4 /dev/sdb2 $LFS
 mkdir -pv $LFS/boot
@@ -12,5 +27,5 @@ mount -v -t ext4 /dev/sdb3 $LFS/home
 
 mkdir -v $LFS/sources
 chmod -v a+wt $LFS/sources
-wget https://www.linuxfromscratch.org/lfs/view/stable/wget-list
-wget --input-file=wget-list --continue --directory-prefix=$LFS/sources
+wget https://www.linuxfromscratch.org/lfs/view/stable/wget-list > wget.out
+wget --input-file=wget-list --continue --directory-prefix=$LFS/sources >> wget.out
