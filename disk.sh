ba81b0d787dea491184fed65fe55a53296f71d34
atweek <ramille655@gmail.com>
2022-10-01 13:29:12 +0300

    disk

--- disk.sh
+++ disk.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+export LFS=/mnt/lfs
+echo $LFS
+
+mkdir -pv $LFS
+mount -v -t ext4 /dev/sdb2 $LFS
+mkdir -pv $LFS/boot
+mount -v -t ext4 /dev/sdb1 $LFS/boot
+mkdir -pv $LFS/home
+mount -v -t ext4 /dev/sdb3 $LFS/home
+
+mkdir -v $LFS/sources
+chmod -v a+wt $LFS/sources
+wget https://www.linuxfromscratch.org/lfs/view/stable/wget-list
+wget --input-file=wget-list --continue --directory-prefix=$LFS/sources
--- first_start.sh
+++ first_start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get -y install binutils bison coreutils diffutils  findutils gawk gcc grep gzip m4 patch perl python3 sed tar texinfo g++ make vim
-sudo ln -sf bash /bin/sh
+# sudo apt-get -y install binutils bison coreutils diffutils  findutils gawk gcc grep gzip m4 patch perl python3 sed tar texinfo g++ make vim fdisk
+# sudo ln -sf bash /bin/sh
+
+export LFS=/mnt/lfs
