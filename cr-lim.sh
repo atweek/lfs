bd56368f17ac840f429904d550b5ea34115d5e95
atweek <ramille655@gmail.com>
2022-10-02 16:55:28 +0300

    disk

--- cr-lim.sh
+++ cr-lim.sh
@@ -1,21 +1,42 @@
-mkdir -pv $LFS/{etc,var} $LFS/usr/{bin,lib,sbin}
+# mkdir -pv $LFS/{etc,var} $LFS/usr/{bin,lib,sbin}
 
-for i in bin lib sbin; do
-  ln -sv usr/$i $LFS/$i
-done
+# for i in bin lib sbin; do
+#   ln -sv usr/$i $LFS/$i
+# done
 
-case $(uname -m) in
-  x86_64) mkdir -pv $LFS/lib64 ;;
-esac
+# case $(uname -m) in
+#   x86_64) mkdir -pv $LFS/lib64 ;;
+# esac
 
-mkdir -pv $LFS/tools
+# mkdir -pv $LFS/tools
 
-groupadd lfs
-useradd -s /bin/bash -g lfs -m -k /dev/null lfs
-passwd lfs
+# groupadd lfs
+# useradd -s /bin/bash -g lfs -m -k /dev/null lfs
+# passwd lfs
 
 chown -v lfs $LFS/{usr{,/*},lib,var,etc,bin,sbin,tools}
 case $(uname -m) in
   x86_64) chown -v lfs $LFS/lib64 ;;
 esac
 su - lfs
+
+cat > ~/.bash_profile << "EOF"
+exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
+EOF
+
+cat > ~/.bashrc << "EOF"
+set +h
+umask 022
+LFS=/mnt/lfs
+LC_ALL=POSIX
+LFS_TGT=$(uname -m)-lfs-linux-gnu
+PATH=/usr/bin
+if [ ! -L /bin ]; then PATH=/bin:$PATH; fi
+PATH=$LFS/tools/bin:$PATH
+CONFIG_SITE=$LFS/usr/share/config.site
+export LFS LC_ALL LFS_TGT PATH CONFIG_SITE
+EOF
+
+export MAKEFLAGS='-j5'
+
+source ~/.bash_profile
