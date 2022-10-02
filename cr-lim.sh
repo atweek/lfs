5defa991ee11f6b9536e0a953b49f9ac1e526405
atweek <ramille655@gmail.com>
2022-10-02 17:09:03 +0300

    disk

--- cr-lim.sh
+++ cr-lim.sh
@@ -1,18 +1,18 @@
-# mkdir -pv $LFS/{etc,var} $LFS/usr/{bin,lib,sbin}
+mkdir -pv $LFS/{etc,var} $LFS/usr/{bin,lib,sbin}
 
-# for i in bin lib sbin; do
-#   ln -sv usr/$i $LFS/$i
-# done
+for i in bin lib sbin; do
+  ln -sv usr/$i $LFS/$i
+done
 
-# case $(uname -m) in
-#   x86_64) mkdir -pv $LFS/lib64 ;;
-# esac
+case $(uname -m) in
+  x86_64) mkdir -pv $LFS/lib64 ;;
+esac
 
-# mkdir -pv $LFS/tools
+mkdir -pv $LFS/tools
 
-# groupadd lfs
-# useradd -s /bin/bash -g lfs -m -k /dev/null lfs
-# passwd lfs
+groupadd lfs
+useradd -s /bin/bash -g lfs -m -k /dev/null lfs
+passwd lfs
 
 chown -v lfs $LFS/{usr{,/*},lib,var,etc,bin,sbin,tools}
 case $(uname -m) in
