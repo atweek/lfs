bc6cb047ef8db1c7be93926d1c0cf8e7fd7792c2
Ramil Suleymanov <ramille655@gmail.com>
2022-10-08 13:35:48 +0300

    Update first_start.sh

--- first_start.sh
+++ first_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo su
 sudo apt-get update -y
 sudo apt-get -y install binutils bison coreutils diffutils  findutils gawk gcc grep gzip m4 patch perl python3 sed tar texinfo g++ make vim fdisk openssh-server
 sudo ln -sf bash /bin/sh
